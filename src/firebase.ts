bd3992b9202d2f7116f3787dc66124c83502ede9
Jorgen <jorgen@buildnode.io>
2021-03-30 23:46:27 -0700

    open firebase connection

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDKLJOl6oBoC9RcczYp7M097Cxot7ubE2U",
+    authDomain: "alaja-c3481.firebaseapp.com",
+    projectId: "alaja-c3481",
+    storageBucket: "alaja-c3481.appspot.com",
+    messagingSenderId: "396042941487",
+    appId: "1:396042941487:web:7441a0b6a7efcbc48a02da",
+    measurementId: "G-QGMEH58DHY"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+
+export { db };
