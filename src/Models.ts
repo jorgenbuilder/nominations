19fcac620861afba3bcf209e6e29bf30bfcbe5a0
Jorgen <jorgen@buildnode.io>
2021-03-31 18:25:36 -0700

    first pass models

--- src/Models.ts
+++ src/Models.ts
@@ -1,8 +1,42 @@
-interface Nomination {}
-interface SongData {}
-interface Round {}
-interface VotSchema {}
-interface NomSchema {}
+interface Nomination {
+    type: 'song' | 'album';
+    data: SongData | AlbumData;
+    votes: Vote[];
+}
+
+interface SongData {
+    spotifyURI: string;
+}
+
+interface AlbumData {
+    spotifyURI: string;
+}
+
+interface Round {
+    status: 'live' | 'archive';
+    nomSchema: NomSchema;
+    votSchema: VotSchema;
+    nominations: Nomination[];
+}
+
+interface VotSchema {
+    [key: number]: number;
+}
+
+interface NomSchema {
+    type: 'song' | 'album';
+    count: number;
+}
+
+interface Vote {
+    points: number;
+    user: User;
+}
+
+interface User {
+    name: string;
+    avatarUrl: string;
+}
 
 export type {
     Nomination,
